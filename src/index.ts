7592ef02bb49ccacdcd4f9816d62a0a1833136de
Yatin darmwal <yatin@infeedo.com>
2020-10-21 22:28:57 +0530

    composition style classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { User } from './models/User';
 
 
-const user = new User({ name: 'NEW NAME', age: 200 });
+const user = new User({ id: 9, name: 'newer name ', age: 67 });
+
+// user.attributes.get('id');
+
+// user.sync.save();
+// user.on('change', () => {
+//     console.log('change triggered');
+// });
+
+user.on('save', () => {
+    console.log(user);
+});
+
 user.save();
-// user.fetch();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,7 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
+import { AxiosResponse } from 'axios';
 
 const rootURl = `http://localhost:3000/users`;
 
@@ -12,4 +14,39 @@ export interface UserProps {
 export class User {
     public events: Eventing = new Eventing();
     public sync: Sync<UserProps> = new Sync<UserProps>(rootURl);
+    public attributes: Attributes<UserProps>;
+    constructor(attrs: UserProps) {
+        this.attributes = new Attributes<UserProps>(attrs);
+    }
+    get on() {
+        return this.events.on;
+    }
+    get trigger() {
+        return this.events.trigger;
+    }
+    get get() {
+        return this.attributes.get;
+    }
+    set(update: UserProps): void {
+        this.attributes.set(update);
+        this.events.trigger('change');
+    }
+    fetch(): void {
+        const id = this.attributes.get('id');
+        if (typeof id != 'number') {
+            throw new Error('Cannot fetch without id ');
+        }
+        this.sync.fetch(id).then((res: AxiosResponse): void => {
+            this.set(res.data);
+        });
+    }
+    save(): void {
+        this.sync.save(this.attributes.getAll())
+            .then((res: AxiosResponse) => {
+                this.trigger('save');
+            })
+            .catch(() => {
+                this.trigger('error');
+            });
+    }
 }
